8b63af9a6de6fbc6e7b27a28d4e288d21f9039c2
vats <vatsalmangukiya9003@gmail.com>
2023-07-03 19:21:33 +0530

    String concatination

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 console.log("Hello World");
 console.log("Gello");
 
+let firstName="Hello";
+let lastName="World";
 
+// String concatination
+let result=firstName+lastName;
+console.log("result:",result);
